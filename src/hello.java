1b7a78e6dee8efde0f482b29132d52d57bf95d25
explorer <u.topia.1998.wang@gmail.com>
2021-06-06 10:07:26 +0800

    第一次提交git文件

---
+++ src/hello.java
@@ -0,0 +1,14 @@
+/**
+ * @author w
+ * @date 2021年06月06日10:02
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
